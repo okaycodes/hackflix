54c5029a66941eda22f784a7270303c42aee6a68
okaycodes <fo.okenana@gmail.com>
2021-11-11 05:00:49 +0100

    update App: create and update Jumbotron component

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,13 @@
+
+
+export default function Jumbotron ({children, direction="row", ...restProps}){
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  )
+
+  Jumbotron.Container({children, ...restProps}){
+    return <Container {...restProps}>{children}</Container>;
+  }
+}
