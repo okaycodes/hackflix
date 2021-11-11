44792e9e39cd9a1cfbff25b85cd1e83f300ee702
okaycodes <fo.okenana@gmail.com>
2021-11-11 20:06:59 +0100

    update App: import and add styled components

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -6,8 +6,16 @@ export default function Jumbotron ({children, direction="row", ...restProps}){
       <Inner direction={direction}>{children}</Inner>
     </Item>
   )
+}
 
-  Jumbotron.Container({children, ...restProps}){
-    return <Container {...restProps}>{children}</Container>;
-  }
+Jumbotron.Container({children, ...restProps}){
+  return <Container {...restProps}>{children}</Container>;
+}
+
+Jumbotron.Title({children, ...restProps}){
+  return <Title {...restProps}>{children}</Title>
+}
+
+Jumbotron.Subtitle({children, ...restProps}){
+  return <SubTitle {...restProps}>{children}</SubTitle>
 }
