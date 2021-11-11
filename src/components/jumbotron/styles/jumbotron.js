b30c20fb57836342f9de82ec04a25de8e944cfcd
okaycodes <fo.okenana@gmail.com>
2021-11-11 20:06:28 +0100

    update App: update jumbotron styled components

---
+++ src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Inner = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const Item = styled.div`
+    align-items: center;
+  `;
+
+export const Container = styled.div`
+    align-items: center;
+`;
