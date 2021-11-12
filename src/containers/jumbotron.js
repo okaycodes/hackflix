a8e6af8d1d8b5cdccb79fc13b7f7d2f8f3c6fc05
okaycodes <fo.okenana@gmail.com>
2021-11-12 06:22:01 +0100

    update App: add and display json data

--- src/containers/jumbotron.js
+++ src/containers/jumbotron.js
@@ -1,10 +1,21 @@
 import Jumbotron from "./../components/jumbotron/index"
+import jumboData from '../fixtures/jumbo.json';
 
 export default function JumbotronContainer(){
   return(
     <Jumbotron.Container>
-      <Jumbotron.Title>Title</Jumbotron.Title>
-      <Jumbotron.SubTitle>Subtitle</Jumbotron.SubTitle>
+      {jumboData.map(item=>{
+        return (
+        <Jumbotron>
+          <Jumbotron.Pane>
+            <Jumbotron.Title>{item.title}</Jumbotron.Title>
+            <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+          </Jumbotron.Pane>
+          <Jumbotron.Pane>
+            <Jumbotron.Image src={item.image} alt={item.alt}/>
+          </Jumbotron.Pane>
+        </Jumbotron>)
+      })}
     </Jumbotron.Container>
   )
 }
