c53ef212a81b74e1875b0c36ad823371070a1c43
okaycodes <fo.okenana@gmail.com>
2021-11-12 06:23:22 +0100

    update Styles

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -3,6 +3,13 @@ import styled from "styled-components";
 export const Inner = styled.div`
     display: flex;
     align-items: center;
+    flex-direction: column;
+    justify-content: space-between;
+
+
+    @media (max-width: 1000) {
+      flex-direction: row;
+    }
 `;
 
 export const Item = styled.div`
@@ -17,6 +24,14 @@ export const Pane = styled.div`
 
 `;
 
-export const Image = styled.div`
+export const Title = styled.h1`
 
-`
+`;
+
+export const SubTitle = styled.h2`
+
+`;
+
+export const Image = styled.img`
+  
+`;
