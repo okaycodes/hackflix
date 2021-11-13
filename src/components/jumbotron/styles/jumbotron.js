6acfcccb5600df03557265fc42880dab1abd8736
okaycodes <fo.okenana@gmail.com>
2021-11-13 05:02:40 +0100

    update App: style Jumbotron

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -1,27 +1,45 @@
 import styled from "styled-components";
 
 export const Inner = styled.div`
-    display: flex;
-    align-items: center;
+  display: flex;
+  align-items: center;
+  /* direction prop is destructured below and returned */
+  flex-direction: ${({direction})=>direction};
+  justify-content: space-between;
+
+  @media (max-width: 1000px) {
     flex-direction: column;
-    justify-content: space-between;
-
-
-    @media (max-width: 1000) {
-      flex-direction: row;
-    }
+  }
 `;
 
 export const Item = styled.div`
-    align-items: center;
+  color: white;
+  padding: 50px 5%;
+  overflow: hidden;
+  align-items: center;
+  border-bottom: 8px solid #222;
+
   `;
 
 export const Container = styled.div`
+  background-color: black;
+
+  @media(max-width: 1000px){
+    /* Item is destructured because it is a child of Container? */
+    ${Item}:last-of-type h2{
+      margin-bottom: 50px;
+    }
+  }
 
 `;
 
 export const Pane = styled.div`
+    width: 50%;
 
+    @media (max-width: 1000px){
+      width: 100%;
+      text-align: center;
+    }
 `;
 
 export const Title = styled.h1`
@@ -33,5 +51,5 @@ export const SubTitle = styled.h2`
 `;
 
 export const Image = styled.img`
-  
+
 `;
