6d613a71a98856c099a31b936f394552d4d5de7b
okaycodes <fo.okenana@gmail.com>
2021-11-13 05:03:15 +0100

    update App: implement Global styles

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
-
+import { GlobalStyles } from './global-styles';
 
 ReactDOM.render(
   <React.StrictMode>
       <Router>
+        <GlobalStyles />
         <App />
       </Router>
   </React.StrictMode>,
