177fdc78f42573ede656de35cc14d914f75aba4d
okaycodes <fo.okenana@gmail.com>
2021-11-14 06:45:30 +0100

    update App: add context and interactivity

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,13 +1,15 @@
 import {useContext} from "react"
 import {Container, Items, Title, Item, Header, SubTitle, Content, Toggle} from "./styles/accordion"
+import {AccordionContextProvider, accordionContext} from "./../../contexts/accordionContext"
 
 
 export default function Accordion({children, ...restProps}){
   return (
+      <AccordionContextProvider>
         <Container {...restProps}>
             {children}
         </Container>
-      )
+      </AccordionContextProvider>)
 }
 
 Accordion.Title = function AccordionTitle({children, ...restProps}){
@@ -22,18 +24,24 @@ Accordion.Item = function AccordionItem({children, ...restProps}){
   return <Item {...restProps}>{children}</Item>
 }
 
-Accordion.Header = function AccordionHeader({children,  ...restProps}){
-  return <Header {...restProps}>{children}</Header>
+
+Accordion.Header = function AccordionHeader({children, index, ...restProps}){
+  const {toggle} = useContext(accordionContext)
+  return <Header onClick={()=>toggle(index)}{...restProps}>{children}</Header>
 }
 
 Accordion.SubTitle = function AccordionSubTitle({children, ...restProps}){
   return <SubTitle {...restProps}>{children}</SubTitle>
 }
 
-Accordion.Content = function AccordionContent({children,  ...restProps}){
-  return <Content {...restProps}>{children}</Content>
+Accordion.Content = function AccordionContent({children, index, ...restProps}){
+  const {activeIndex} = useContext(accordionContext)
+  const isActive = activeIndex === index
+  return isActive && <Content {...restProps}>{children}</Content>
 }
 
-Accordion.Toggle = function AccordionToggle({children,  ...restProps}){
-  return <Toggle  {...restProps}>{children}</Toggle>
+Accordion.Toggle = function AccordionToggle({children, index, ...restProps}){
+  const {activeIndex} = useContext(accordionContext)
+  const isActive = activeIndex === index
+  return <Toggle isActive={isActive} {...restProps}>{children}</Toggle>
 }
