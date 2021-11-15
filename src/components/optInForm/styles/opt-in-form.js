3f49508f94cf6a200784085e708553ca5bb1ff72
okaycodes <fo.okenana@gmail.com>
2021-11-15 09:09:23 +0100

    create and update styles for opt-in-form

---
+++ src/components/optInForm/styles/opt-in-form.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+
+`;
+
+export const Message = styled.p`
+
+`;
+
+export const Input = styled.input`
+
+`;
+
+export const Button = styled.button`
+
+`;
