2b09c63f3e1adc76d348fbcd95ea7e3f5044ad44
okaycodes <fo.okenana@gmail.com>
2021-11-16 13:47:09 +0100

    update Styles: styled input field

--- src/components/opt-in-form/styles/opt-in-form.js
+++ src/components/opt-in-form/styles/opt-in-form.js
@@ -1,7 +1,8 @@
 import styled from "styled-components"
 
 export const Container = styled.div`
-
+  display: flex;
+  flex-direction: column;
 `;
 
 export const Message = styled.p`
@@ -9,34 +10,61 @@ export const Message = styled.p`
 `;
 
 export const Wrapper = styled.div`
-
+  display: flex;
+  flex-direction: column;
 `;
 
 
-export const Placeholder = styled.label`
-  display: none;
+export const InputWrapper = styled.div`
+  position: relative;
+  --size: 16px;
+`;
+
+export const Label = styled.label`
+
+`;
+
+export const Placeholder = styled.span`
+  font-size: 16px;
+  color: grey;
+  transition: all .15s ease-in-out;
 `;
 
 export const Input = styled.input`
-  font-size: 16px;
+  position: absolute;
+  font-size: var(--size);
+  padding: 1em;
+  padding-left: .7em;
   margin: 0;
   width: 400px;
-  height: 50px;
+  outline: none;
 
-  &:focus::placeholder{
-    font-size: 0;
+  &:focus{
+    border: none;
+    border-bottom: solid 2px orange;
+  }
+
+  & ~ ${Placeholder}{
+    position: absolute;
+    top: var(--size);
+    left: 10px;
   }
 
   &:focus + ${Placeholder},
   &:not(:placeholder-shown) + ${Placeholder}{
-    display: inline-block;
     font-size: 12px;
+    font-weight: bold;
+    top: 0;
     color: grey;
-    transform: translate(-395px, -16px);
     user-select: none;
   }
 `;
 
+
 export const Button = styled.button`
 
+`;
+
+export const Image = styled.img`
+  
 `;
