95d984b7e3b1e2dc802441c1e61e7fa199885bb9
okaycodes <fo.okenana@gmail.com>
2021-11-17 22:48:59 +0100

    update App: header styles

--- src/components/header/style/header.js
+++ src/components/header/style/header.js
@@ -3,21 +3,61 @@ import {Link as ReachRouterLink} from  "react-router-dom"
 
 export const Background = styled.div`
   display: flex;
-  flex-direction: column
-`;
+  flex-direction: column;
+  background: url(${({src}) => src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg'});
 
-export const Frame = styled.div`
+  @media (min-width: 1100px) {
+    ${({dontShowOnSmallViewPort})=> dontShowOnSmallViewPort && "background: none"}
+  }
 `;
 
 export const Container = styled.div`
+  box-sizing: content-box;
+  display: flex;
+  margin: 0 56px;
+  height: 64px;
+  padding: 18px 0;
+  justify-content: space-between;
+  align-items: center;
+
+  a{
+      display: flex;
+  }
+
+  @media (max-width: 1000px) {
+      margin: 0 30px;
+  }
 `;
 
 export const Logo = styled.img`
+  height: 32px;
+  width: 108px;
+  margin-right: 40px;
+
+  @media (min-width: 1449px) {
+     height: 45px;
+     width: 167px;
+  }
 `;
 
 export const Link = styled.p`
 `;
 
 export const ButtonLink = styled(ReachRouterLink)`
+display: block;
+  background-color: #e50914;
+  width: 84px;
+  height: fit-content;
+  color: white;
+  border: 0;
+  font-size: 15px;
+  border-radius: 3px;
+  padding: 8px 17px;
+  cursor: pointer;
+  text-decoration: none;
+  box-sizing: border-box;
 
+  &:hover {
+      background: #f40612;
+  }
 `;
