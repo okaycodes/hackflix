dcbd3667034ff474e2f54f749474a089f9bca3a0
okaycodes <fo.okenana@gmail.com>
2021-11-17 22:48:36 +0100

    update App: header component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
-import {Container, Background, Logo, Link} from "./style/header"
-import {Link as ReachRouterLink} from "react-router-dom"
+import {Container, Background, Logo, ButtonLink} from "./style/header"
+import * as ROUTES from './../../constants/routes';
+import {Link as ReactRouterLink} from "react-router-dom"
 
 export default function Header({bg=true, children, ...restProps}){
   return bg ? <Background {...restProps}>{children}</Background> : children
@@ -9,6 +10,10 @@ Header.Frame = function HeaderFrame({children, ...restProps}){
   return <Container>{children}</Container>
 }
 
-Header.Logo = function HeaderLogo({children, to, ...restProps}){
-  return <ReachRouterLink to={to}><Logo {...restProps}/></ReachRouterLink>
+Header.Logo = function HeaderLogo({children, ...restProps}){
+  return <ReactRouterLink to={ROUTES.HOME}><Logo {...restProps}/></ReactRouterLink>
+}
+
+Header.ButtonLink = function HeaderButtonLink({children, ...restProps}){
+  return <ButtonLink to={ROUTES.SIGN_IN}>Sign In</ButtonLink>
 }
