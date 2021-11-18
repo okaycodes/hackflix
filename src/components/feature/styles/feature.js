bcc4b61736741c9de719f297db5102d8e34933ad
okaycodes <fo.okenana@gmail.com>
2021-11-18 04:02:52 +0100

    create and update: feature component

---
+++ src/components/feature/styles/feature.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const Container = styled.section`
+`;
+
+export const Title = styled.h1`
+`;
+
+export const SubTitle = styled.h2`
+`;
+
+export const Text = styled.p`
+`
