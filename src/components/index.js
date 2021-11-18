3c558711f4fcb97da35e0f0941515b593c2e2d9f
okaycodes <fo.okenana@gmail.com>
2021-11-18 04:03:43 +0100

    update App: render pages

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 export {default as OptInForm} from "./opt-in-form";
+export {default as Header} from "./header";
+export {default as Feature} from "./feature"
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
 import JumbotronContainer from "./../containers/jumbotron"
 import FooterContainer from "./../containers/footer"
 import AccordionContainer from "./../containers/accordion"
+import HeaderContainer from "./../containers/header"
+import {Feature} from "./../components"
 
 export default function Home (){
   return (
     <>
+      <HeaderContainer>
+        <Feature />
+      </HeaderContainer>
       <JumbotronContainer />
       <AccordionContainer />
       <FooterContainer />
