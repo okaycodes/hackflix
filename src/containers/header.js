b560743e9d8274067aa8c0f5af55ae9e7445d6ae
okaycodes <fo.okenana@gmail.com>
2021-11-18 04:00:26 +0100

    update App: Header container

---
+++ src/containers/header.js
@@ -0,0 +1,14 @@
+import {Header} from "./../components"
+
+
+export default function HeaderContainer(children){
+  return(
+    <Header bg="false">
+      <Header.Frame>
+        <Header.Logo src="./../../images/misc/logo.svg" alt="Netflix" />
+        <Header.ButtonLink />
+      </Header.Frame>
+      {children}
+    </Header>
+  )
+}
