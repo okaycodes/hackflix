9d4c0af8797c7465964b22fa834a96f15dde2bb3
okaycodes <fo.okenana@gmail.com>
2021-11-19 19:47:11 +0100

    import Form.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export {default as OptInForm} from "./opt-in-form";
 export {default as Header} from "./header";
 export {default as Feature} from "./feature"
+export {default as Form} from "./form"
