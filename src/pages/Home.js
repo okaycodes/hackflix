e3153a3183a3cc7cd61187be2538c16e35fda715
okaycodes <fo.okenana@gmail.com>
2021-11-19 05:08:09 +0100

    update App: rendered feature in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,19 +2,23 @@ import JumbotronContainer from "./../containers/jumbotron"
 import FooterContainer from "./../containers/footer"
 import AccordionContainer from "./../containers/accordion"
 import HeaderContainer from "./../containers/header"
-import {OptInForm} from "./../components"
+import {OptInForm, Feature} from "./../components"
 
 
 export default function Home (){
   return (
     <>
       <HeaderContainer>
-        <OptInForm>
-          <OptInForm.Message>Ready to watch? Enter your email to create or restart your
-           membership.</OptInForm.Message>
-          <OptInForm.Input />
-          <OptInForm.Button>Get Started</OptInForm.Button>
-        </OptInForm>
+        <Feature>
+          <Feature.Title>Unlimited movies, TV shows and more.</Feature.Title>
+          <Feature.SubTitle>Watch anywhere. Cancel at any time.</Feature.SubTitle>
+          <OptInForm>
+            <OptInForm.Message>Ready to watch? Enter your email to create or restart your
+             membership.</OptInForm.Message>
+            <OptInForm.Input placeholder="Email address"/>
+            <OptInForm.Button>Get Started</OptInForm.Button>
+          </OptInForm>
+        </Feature>
       </HeaderContainer>
       <JumbotronContainer />
       <AccordionContainer />
