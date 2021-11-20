89330ade2c8c04bbf1bdba23fadbc4d958dca515
okaycodes <fo.okenana@gmail.com>
2021-11-20 23:13:00 +0100

    update App: optInContext

--- src/contexts/optInContext.js
+++ src/contexts/optInContext.js
@@ -3,10 +3,20 @@ import React, {useState} from "react"
 const optInContext = React.createContext()
 
 function OptInContextProvider({children}){
-  
+  const [email, setEmail] = useState('')
+  const isEmpty = email.length < 1
+  const emailIsValid = /[^@]+@[^@]+\.[^@]+/.test(email)
+  const [activatedInput, setActivatedInput] = useState(false)
+
+  const handleChange =(event)=>{
+    event.preventDefault()
+    const {value} = event.target
+    setEmail(value)
+    setActivatedInput(true)
+  }
 
   return (
-    <optInContext.Provider value={}>
+    <optInContext.Provider value={{email, emailIsValid, activatedInput, handleChange, isEmpty}}>
       {children}
     </optInContext.Provider>
   )
