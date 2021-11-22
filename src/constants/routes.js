d36645ad5ef02a2a1c6938f5db747e017cc39f3e
okaycodes <fo.okenana@gmail.com>
2021-11-22 17:46:08 +0100

    update constants file

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,9 @@
 export const HOME = '/';
 export const BROWSE = '/browse';
-export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
+export const SIGN_UP = '/signup/*';
+export const REG_FORM = 'regform';
+export const PLAN_FORM = '/signup/planform';
 
 /* defining routes in constant allows us to avoid hardcoding urls all over our
 app. this way we can easily change the path and have it reflect properly all over our app*/
