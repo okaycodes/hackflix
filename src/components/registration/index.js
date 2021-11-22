914fdaa3a469620aa0610f8e1b892b6e07b58594
okaycodes <fo.okenana@gmail.com>
2021-11-22 17:45:24 +0100

    update App: registration component and styles

--- src/components/registration/index.js
+++ src/components/registration/index.js
@@ -1,27 +1,27 @@
-import {Container, Image, Title, SubTitle, Text, Link} from "./styles/registration"
+import {Container, Inner, Image, Title, SubTitle, Text, Link} from "./styles/registration"
 
 
 export default function Registration({children, ...restProps}){
-  return <Container {...restProps}>{children}</Container>
+  return <Container {...restProps}><Inner>{children}</Inner></Container>
 }
 
 
-Registration.Image = RegistrationImage({children, ...restProps}){
+Registration.Image = function RegistrationImage({children, ...restProps}){
   return <Image {...restProps} />
 }
 
-Registration.Title = RegistrationTitle({children, ...restProps}){
+Registration.Title = function RegistrationTitle({children, ...restProps}){
   return <Title {...restProps}>{children}</Title>
 }
 
-Registration.SubTitle = RegistrationSubTitle({children, ...restProps}){
+Registration.SubTitle = function RegistrationSubTitle({children, ...restProps}){
   return <SubTitle {...restProps}>{children}</SubTitle>
 }
 
-Registration.Text = RegistrationText({children, ...restProps}){
+Registration.Text = function RegistrationText({children, ...restProps}){
   return <Text {...restProps}>{children}</Text>
 }
 
-Registration.Link = RegistrationLink({children, ...restProps}){
+Registration.Link = function RegistrationLink({children, ...restProps}){
   return <Link {...restProps}>{children}</Link>
 }
--- src/components/registration/styles/registration.js
+++ src/components/registration/styles/registration.js
@@ -3,19 +3,61 @@ import {Link as ReactRouterLink} from "react-router-dom"
 
 
 export const Container = styled.div`
+  display: flex:
+  flex-direction: column;
+  background: white;
+  width: 100vw;
+  border-bottom: solid 1px lightgrey;
+  border-top: solid 1px lightgrey;
 `;
 
+export const Inner = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  max-width: 340px;
+  justify-content: center;
+  align-items: center;
+  margin: 140px auto;
+`
+
 export const Image = styled.img`
+  margin-bottom: 1em;
+  display: block;
+  width: 100%;
+  order: -2;
 `;
 
 export const Title = styled.h1`
+  font-size: 23px;
+  font-weight: 600;
+  margin: 0;
 `;
 
 export const SubTitle = styled.p`
+  font-size: 13px;
+  text-transform: uppercase;
+  order: -1;
 `;
 
 export const Text = styled.p`
+  text-align: center;
+  font-size: 17px;
+  font-weight: 400;
 `;
 
-export const Link = styled.(ReactRouterLink)`
+export const Link = styled(ReactRouterLink)`
+  width: 100%;
+  color: white;
+  background-color: #e50914;
+  padding: 1em;
+  font-size: 16px;
+  text-decoration: none;
+  text-align: center;
+  border-radius: 3px;
+
+
+  &:hover{
+    background: #f40612;
+  }
 `;
