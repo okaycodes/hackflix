5a6403311f63d446a835d33fbf8ad490b8b7ab67
okaycodes <fo.okenana@gmail.com>
2021-11-22 17:45:49 +0100

    export components

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export {default as OptInForm} from "./opt-in-form";
 export {default as Header} from "./header";
+export {default as Footer} from "./footer";
 export {default as Feature} from "./feature"
 export {default as Form} from "./form"
+export {default as Registration} from "./registration"
