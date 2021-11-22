403b8dfdf50d23f09208a8f02cca0b78b54b09de
okaycodes <fo.okenana@gmail.com>
2021-11-22 17:36:34 +0100

    update App: signup page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,5 +1,7 @@
-import {Header} from "./../components"
+import {Header, Footer} from "./../components"
 import {Routes, Route} from 'react-router-dom'
+import RegistrationContainer from "./../containers/registration"
+import * as ROUTES from "./../constants/routes"
 
 export default function SignUp({children}){
   return (
@@ -12,7 +14,22 @@ export default function SignUp({children}){
         </Header.Frame>
       </Header>
 
-      
+    <Routes>
+      <Route exact path="/" element={<RegistrationContainer />}/>
+      <Route exact path=":regform" element={<>this is the regform page</>} />
+    </Routes>
+
+    <Footer backgroundColor="#ededed" padding="2em 3em">
+      <Footer.Title>Questions? Contact Us</Footer.Title>
+      <Footer.Row>
+        <Footer.Link href="#">FAQs</Footer.Link>
+        <Footer.Link href="#">Help Center</Footer.Link>
+        <Footer.Link href="#">Terms of Use</Footer.Link>
+        <Footer.Link href="#">Privacy</Footer.Link>
+        <Footer.Link href="#">Cookie Preferences</Footer.Link>
+        <Footer.Link href="#">Corporate Information</Footer.Link>
+      </Footer.Row>
+    </Footer>
     </>
   )
 }
