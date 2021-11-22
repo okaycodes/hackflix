667bd5723f2009d2f36f5212b1b8a34bd2888814
okaycodes <fo.okenana@gmail.com>
2021-11-22 17:47:20 +0100

    create and update: registrationContainer

---
+++ src/containers/registration.js
@@ -0,0 +1,17 @@
+import {Registration} from "./../components"
+import * as ROUTES from "./../constants/routes"
+
+
+export default function RegistrationContainer(){
+  return(
+  <Registration>
+    <Registration.Title>Finish setting up your account</Registration.Title>
+    <Registration.SubTitle>
+      step <span style={{fontWeight: "bold"}}>1</span> of <span style={{fontWeight: "bold"}}>3</span>
+    </Registration.SubTitle>
+    <Registration.Image src="./../../images/icons/devices.png" alt="Devices"/>
+    <Registration.Text>Netflix is personalized for you. Create a
+    password to watch on any device at any time.</Registration.Text>
+    <Registration.Link to={ROUTES.REG_FORM}>Next</Registration.Link>
+  </Registration>
+)}
