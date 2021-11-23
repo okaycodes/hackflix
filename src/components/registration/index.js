a7beb1f2d5083ffd31c6c74a58045ce328472037
okaycodes <fo.okenana@gmail.com>
2021-11-23 21:15:57 +0100

    update App: registration component

--- src/components/registration/index.js
+++ src/components/registration/index.js
@@ -1,13 +1,40 @@
-import {Container, Inner, Image, Title, SubTitle, Text, Link} from "./styles/registration"
+import {useContext} from "react"
+import {InputContextProvider, inputContext} from "./../../contexts/inputContext"
+import {Container,
+        Inner,
+        Icon,
+        Image,
+        Title,
+        SubTitle,
+        Text,
+        Link,
+        Label,
+        InputWrapper,
+        Input,
+        Placeholder,
+        ErrorMessage,
+        CheckboxWrapper,
+        Checkbox,
+        List,
+        ListItem} from "./styles/registration"
 
 
 export default function Registration({children, ...restProps}){
-  return <Container {...restProps}><Inner>{children}</Inner></Container>
-}
+  return(
+    <InputContextProvider>
+      <Container {...restProps}>
+        <Inner {...restProps}>{children}</Inner>
+      </Container>
+    </InputContextProvider>
+)}
 
 
 Registration.Image = function RegistrationImage({children, ...restProps}){
   return <Image {...restProps} />
+}
+
+Registration.Icon = function RegistrationIcon({children, ...restProps}){
+  return <Icon {...restProps} />
 }
 
 Registration.Title = function RegistrationTitle({children, ...restProps}){
@@ -24,4 +51,40 @@ Registration.Text = function RegistrationText({children, ...restProps}){
 
 Registration.Link = function RegistrationLink({children, ...restProps}){
   return <Link {...restProps}>{children}</Link>
+}
+
+Registration.Input = function OptInFormInput({children, animated=true, placeholder, ...restProps}){
+  const {email, handleChange, activatedInput, emailIsValid, isEmpty} = useContext(inputContext)
+  return(
+    <InputWrapper {...restProps}>
+      <Label>
+        <Input
+          placeholder={!animated && placeholder}
+          name = "email"
+          value ={email}
+          onChange={handleChange}
+        />
+        <Placeholder isEmpty = {isEmpty}>{animated && placeholder}</Placeholder>
+      </Label>
+      <ErrorMessage>
+        {(activatedInput && email.length < 4) ? "Email is required" :
+         activatedInput && !emailIsValid && "Please enter a valid email address"}
+      </ErrorMessage>
+    </InputWrapper>
+  )}
+
+Registration.Checkbox = function RegistrationCheckbox({children, ...restProps}){
+  return(
+    <CheckboxWrapper> Please do not email me Netflix special offers.
+      <Checkbox type="checkbox" {...restProps}>{children}</Checkbox>
+      <span></span>
+    </CheckboxWrapper>
+)}
+
+Registration.List = function RegistrationList({children, ...restProps}){
+  return <List {...restProps}>{children}</List>
+}
+
+Registration.ListItem = function RegistrationListItem({children, ...restProps}){
+  return <ListItem {...restProps}>{children}</ListItem>
 }
