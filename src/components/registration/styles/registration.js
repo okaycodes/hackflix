68b68f746511d650c3d2e2809d2c34b3fbfb9e02
okaycodes <fo.okenana@gmail.com>
2021-11-23 21:17:50 +0100

    update Styles: added Icon and List Items for regform design

--- src/components/registration/styles/registration.js
+++ src/components/registration/styles/registration.js
@@ -15,17 +15,25 @@ export const Inner = styled.div`
   display: flex;
   flex-direction: column;
   width: 90%;
-  max-width: 340px;
   justify-content: center;
   align-items: center;
   margin: 140px auto;
-`
+  max-width: ${props=>props.small ? "340px" : "440px"};
+`;
 
 export const Image = styled.img`
   margin-bottom: 1em;
   display: block;
   width: 100%;
-  order: -2;
+  order: ${props=>props.order};
+`;
+
+export const Icon = styled.img`
+  display: inline-block;
+  filter: invert(11%) sepia(74%) saturate(6242%) hue-rotate(352deg) brightness(104%) contrast(104%);
+  order: ${props=>props.order};
+  height: ${props=>props.height};
+  width: ${props=>props.width};
 `;
 
 export const Title = styled.h1`
@@ -35,16 +43,127 @@ export const Title = styled.h1`
 `;
 
 export const SubTitle = styled.p`
+  width: 100%;
   font-size: 13px;
   text-transform: uppercase;
-  order: -1;
+  order: ${props=>props.order};
+  text-align: ${props=> props.alignLeft ? "left" : "center"};
 `;
 
 export const Text = styled.p`
-  text-align: center;
   font-size: 17px;
   font-weight: 400;
+  text-align: ${props=> props.alignLeft ? "left" : "center"};
+  margin: ${props=>props.margin};
 `;
+
+export const InputWrapper = styled.div`
+  width: 100%;
+  max-width: 450px;
+  position: relative;
+  margin-top: .6em;
+  --size: 20px;
+`;
+
+export const Label = styled.label`
+`;
+
+export const Placeholder = styled.span`
+  font-size: ${props => props.isEmpty ? "16px" : "11px"};
+  font-weight: ${props => !props.isEmpty && "500"};
+  position: absolute;
+  top: var(--size);
+  left: var(--size);
+  ${props => !props.isEmpty && "top: 4px"};
+  color: grey;
+  transition: all .15s ease-in-out;
+  /* props is used to move the placeholder top when the field is not empty
+  top is also declared twice to allow for use of the --size variable for
+  easier edits in the future
+  */
+`;
+
+export const Input = styled.input`
+  display: block;
+  font-size: 16px;
+  padding: 1em;
+  padding-left: 20px;
+  margin: 0;
+  width: 100%;
+  height: 60px;
+  border: solid grey 1px;
+  border-radius: 2px;
+
+   &:focus{
+     outline: solid blue .5px;
+   }
+
+  &:focus +  ${Placeholder}{
+    font-size: 12px;
+    font-weight: 600;
+    top: 4px;
+    color: grey;
+    user-select: none;
+  }
+`;
+
+export const ErrorMessage = styled.div`
+  font-size: 14px;
+  text-align: left;
+  margin-left: .4em;
+  color: red;
+`;
+
+export const CheckboxWrapper = styled.label`
+  display: block;
+  position: relative;
+  padding-left: 35px;
+  margin-top: 25px;
+  cursor: pointer;
+  font-size: 16px;
+  align-self: start;
+  height: 32px;
+`;
+
+export const Checkbox = styled.input`
+  position: absolute;
+  opacity: 0;
+  height: 0;
+  width: 0;
+  cursor: pointer;
+
+  & ~ span{
+    position: absolute;
+    top: -5px;
+    /* top is set to negative to better center the box */
+    left: 0;
+    height: 27px;
+    width: 27px;
+    border: solid 1px black;
+  }
+
+  ${CheckboxWrapper} &:checked~span{
+    background-color: white;
+  }
+
+  & ~ span:after{
+    display: none;
+    content: "";
+    position: absolute;
+    height: 8px;
+    width: 15px;
+    top: 3.5px;
+    left: 2.5px;
+    border: solid #2983f0;
+    border-width: 4px 4px 0 0;
+    transform: rotate(135deg);
+  }
+
+  ${CheckboxWrapper} &:checked~span:after{
+    display: block;
+  }
+`;
+
 
 export const Link = styled(ReactRouterLink)`
   width: 100%;
@@ -55,9 +174,19 @@ export const Link = styled(ReactRouterLink)`
   text-decoration: none;
   text-align: center;
   border-radius: 3px;
+  margin-top: 20px;
 
 
   &:hover{
     background: #f40612;
   }
+`;
+
+export const List = styled.ul`
+  list-style: none;
+  padding: 0;
+`;
+
+export const ListItem = styled.li`
+  display: flex;
 `;
