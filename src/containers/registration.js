3a9f0fbe50f1f077718e48f51afd7171deaf3db7
okaycodes <fo.okenana@gmail.com>
2021-11-23 21:19:19 +0100

    update App: registration container

--- src/containers/registration.js
+++ src/containers/registration.js
@@ -4,14 +4,14 @@ import * as ROUTES from "./../constants/routes"
 
 export default function RegistrationContainer(){
   return(
-  <Registration>
+  <Registration small={true}>
     <Registration.Title>Finish setting up your account</Registration.Title>
-    <Registration.SubTitle>
+    <Registration.SubTitle order="-1">
       step <span style={{fontWeight: "bold"}}>1</span> of <span style={{fontWeight: "bold"}}>3</span>
     </Registration.SubTitle>
-    <Registration.Image src="./../../images/icons/devices.png" alt="Devices"/>
+    <Registration.Image order="-2" src="./../../images/icons/devices.png" alt="Devices"/>
     <Registration.Text>Netflix is personalized for you. Create a
     password to watch on any device at any time.</Registration.Text>
-    <Registration.Link to={ROUTES.REG_FORM}>Next</Registration.Link>
+    <Registration.Link to={`../../${ROUTES.REG_FORM}`}>Next</Registration.Link>
   </Registration>
 )}
