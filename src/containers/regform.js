9e84f605cea00be2883716d2ccae9a738cfd0ce1
okaycodes <fo.okenana@gmail.com>
2021-11-24 23:19:33 +0100

    update App: regform

--- src/containers/regform.js
+++ src/containers/regform.js
@@ -1,7 +1,18 @@
 import {Registration} from "./../components"
 import * as ROUTES from "./../constants/routes"
+import {useState, useEffect} from "react"
+import {inputContext} from  "./../contexts/inputContext"
+import {useContext} from "react"
 
 export default function RegFormContainer(){
+  const {email, setEmail,
+        password, setPassword,
+        emailIsValid, setEmailIsValid,
+        passwordIsValid, setPasswordIsValid,
+        emailIsActivated, setEmailIsActivated,
+        passwordIsActivated, setPasswordIsActivated,
+        emailErrorMessage, passwordErrorMessage} = useContext(inputContext)
+
   return(
     <Registration>
       <Registration.Title>Create a password to start your membership</Registration.Title>
@@ -10,8 +21,37 @@ export default function RegFormContainer(){
       </Registration.SubTitle>
       <Registration.Text alignLeft={true}>Just a few more steps and you're done!
       We hate paperwork, too.</Registration.Text>
-      <Registration.Input placeholder="Email address"/>
-      <Registration.Input placeholder="Add a password"/>
+      <Registration.Input
+        placeholder="Email address"
+        name="email"
+        value={email}
+        isValid ={emailIsValid}
+        isEmpty = {email.length < 1}
+        isActivated={emailIsActivated}
+        errorMessage={emailErrorMessage}
+        onChange={(event)=>{
+          setEmail(event.target.value)
+        }}
+        onBlur={(event)=>{
+          setEmailIsActivated(email.length>=1 && true)
+        }}
+      />
+
+      <Registration.Input
+        placeholder="Add a password"
+        name="password"
+        value={password}
+        isValid ={passwordIsValid}
+        isEmpty={password.length < 1}
+        isActivated={passwordIsActivated}
+        errorMessage={passwordErrorMessage}
+        onChange={(event)=>{
+          setPassword(event.target.value)
+        }}
+        onBlur={(event)=>{
+          setPasswordIsActivated(password.length>=1 && true)
+        }}
+      />
       <Registration.Checkbox />
       <Registration.Link to={ROUTES.SIGN_UP_HOME}>Next</Registration.Link>
     </Registration>
