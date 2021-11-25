5ffd2a29daff2cdf1ed2b8f41326d757f01b03a0
okaycodes <fo.okenana@gmail.com>
2021-11-25 22:03:10 +0100

    update Styles: planform

--- src/components/planform/styles/planform.js
+++ src/components/planform/styles/planform.js
@@ -10,44 +10,78 @@ export const Container = styled.section`
 `;
 
 export const Inner = styled.div`
+  position: relative;
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
   margin: 0 auto;
   width: 90%;
   max-width: 946px;
-
+  background: none;
 `;
 
-export const Item = styled.div`
-  /* border: solid magenta; */
+export const Header = styled.div`
+  display: grid;
+  grid-template-columns:  300px repeat(4, 1fr);
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  justify-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 18px;
+  width: 120px;
+  height: 120px;
   display: flex;
-  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: red;
+  color: white;
+  border-radius: 3px;
+  color: ${props=>props.color};
   text-align: center;
-  &:nth-child(1){
-    text-align: left;
+  opacity: ${props=>props.isActive ? "1" : ".7"};
+  position: static;
+  z-index: 1;
+
+  &:before{
+    content: "";
+    position: absolute;
+    /* background: green; */
+    width: 150px;
+    opacity: 0.1;
+    bottom: 0;
+    top: 0px;
   }
 
-  &:nth-child(1):nth-child(1){
+  &:nth-child(1){
     background: none;
   }
 `;
 
-export const Title = styled.h1`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 150px;
-  height: 150px;
-  margin: 0 auto;
-`;
-
-export const Row = styled.p`
-  border-bottom: solid 1px grey;
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns:  300px repeat(4, 1fr);
+  border-bottom: solid 1.5px grey;
   &:nth-child(5){
     border-bottom: none;
   }
 `;
 
+export const Item = styled.div`
+  color: ${props=>props.color};
+  text-align: center;
+  padding: 1em;
+  font-weight: 600;
+  opacity: .6;
+
+  &:nth-child(1){
+    text-align: left;
+    opacity: 1;
+    font-weight: 400;
+  }
+`;
+
 export const Icon = styled.img`
+  width: 100%;
   filter: invert(11%) sepia(74%) saturate(6242%) hue-rotate(352deg) brightness(104%) contrast(104%);
 `;
