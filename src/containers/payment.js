7e1738052a99e02dc7bf064b3bb70f829c6cd341
okaycodes <fo.okenana@gmail.com>
2021-11-28 22:55:13 +0100

    update App: payment js

--- src/containers/payment.js
+++ src/containers/payment.js
@@ -4,13 +4,23 @@ import * as ROUTES from "./../constants/routes"
 
 export default function PaymentStepContainer(){
   return(
-    <Registration>
+    <Registration style={{textAlign: "center", justifyContent: "center"}}>
       <Registration.Title>Set your payment</Registration.Title>
-      <Registration.SubTitle style={{textAlign: "center"}}>
+      <Registration.SubTitle>
         step <span style={{fontWeight: "bold"}}>3</span> of <span style={{fontWeight: "bold"}}>3</span>
       </Registration.SubTitle>
       <Registration.Icon  style={{width:"45px"}}  src="./../../images/icons/lock.png"/>
-      <Registration.Text>Your membership starts as soon as you set up payment.</Registration.Text>
-      <Registration.Text>No commitments.<br />Cancel online anytime.</Registration.Text>
+      <Registration.Text >
+        Your membership starts as<br/> soon as you set up<br/> payment.
+      </Registration.Text>
+      <Registration.Text style={{fontWeight: "bold"}}>
+        No commitments.<br />Cancel online anytime.
+      </Registration.Text>
+      <Registration.LinkSecondary to="#">
+          Credit or Debit Card
+          <Registration.IconSecondary src="./../../images/icons/visa-card.png" style={{padding: "6px 2.5px"}}/>
+          <Registration.IconSecondary src="./../../images/icons/mastercard_icon.png"/>
+          <Registration.IconSecondary src="./../../images/icons/icon_verve.png"/>
+      </Registration.LinkSecondary>
     </Registration>
   )}
