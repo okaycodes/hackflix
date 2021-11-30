5471a83603a0df45f7cb413361af02dea40e38fa
okaycodes <fo.okenana@gmail.com>
2021-11-30 22:05:16 +0100

    update App: payment form

--- src/containers/paymentform.js
+++ src/containers/paymentform.js
@@ -4,5 +4,10 @@ import * as ROUTES from "./../constants/routes"
 
 export default function PaymentFormContainer(){
   return(
-    <h1>This is the payment form container page</h1>
+    <Registration style={{alignItems: "start"}}>
+      <Registration.Title>Set up your credit or debit card</Registration.Title>
+      <Registration.SubTitle>
+        step <span style={{fontWeight: "bold"}}>3</span> of <span style={{fontWeight: "bold"}}>3</span>
+      </Registration.SubTitle>
+    </Registration>
   )}
