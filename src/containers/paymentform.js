7a19bc1034e85ae549604a073f0d9386268ce7a0
okaycodes <fo.okenana@gmail.com>
2021-11-30 22:06:05 +0100

    update App: paymentform

--- src/containers/paymentform.js
+++ src/containers/paymentform.js
@@ -9,5 +9,10 @@ export default function PaymentFormContainer(){
       <Registration.SubTitle>
         step <span style={{fontWeight: "bold"}}>3</span> of <span style={{fontWeight: "bold"}}>3</span>
       </Registration.SubTitle>
+      <Registration.IconWrapper>
+        <Registration.IconSecondary src="./../../images/icons/visa-card.png" style={{padding: "6px 2.5px"}}/>
+        <Registration.IconSecondary src="./../../images/icons/mastercard_icon.png"/>
+        <Registration.IconSecondary src="./../../images/icons/icon_verve.png"/>
+      </Registration.IconWrapper>
     </Registration>
   )}
