f0ca9ba5b805407a9218fd69061944e049686bc7
okaycodes <fo.okenana@gmail.com>
2021-11-30 22:10:23 +0100

    update App:add context and input fields

--- src/containers/paymentform.js
+++ src/containers/paymentform.js
@@ -1,8 +1,17 @@
 import {Registration} from "./../components"
 import * as ROUTES from "./../constants/routes"
+import {inputContext} from  "./../contexts/inputContext"
+import {useContext} from "react"
 
 
 export default function PaymentFormContainer(){
+  const {email, setEmail,
+        password, setPassword,
+        emailIsValid, passwordIsValid,
+        emailIsActivated, setEmailIsActivated,
+        passwordIsActivated, setPasswordIsActivated,
+        emailErrorMessage, passwordErrorMessage} = useContext(inputContext)
+
   return(
     <Registration style={{alignItems: "start"}}>
       <Registration.Title>Set up your credit or debit card</Registration.Title>
@@ -14,5 +23,80 @@ export default function PaymentFormContainer(){
         <Registration.IconSecondary src="./../../images/icons/mastercard_icon.png"/>
         <Registration.IconSecondary src="./../../images/icons/icon_verve.png"/>
       </Registration.IconWrapper>
+      <Registration.Input
+        placeholder="Email address"
+        name="email"
+        value={email}
+        isValid ={emailIsValid}
+        isEmpty = {email.length < 1}
+        isActivated={emailIsActivated}
+        errorMessage={emailErrorMessage}
+        onChange={(event)=>{
+          setEmail(event.target.value)
+        }}
+        onBlur={(event)=>{
+          setEmailIsActivated(email.length>=1 && true)
+        }}
+      />
+      <Registration.Input
+        placeholder="Email address"
+        name="email"
+        value={email}
+        isValid ={emailIsValid}
+        isEmpty = {email.length < 1}
+        isActivated={emailIsActivated}
+        errorMessage={emailErrorMessage}
+        onChange={(event)=>{
+          setEmail(event.target.value)
+        }}
+        onBlur={(event)=>{
+          setEmailIsActivated(email.length>=1 && true)
+        }}
+      />
+      <Registration.Input
+        placeholder="Email address"
+        name="email"
+        value={email}
+        isValid ={emailIsValid}
+        isEmpty = {email.length < 1}
+        isActivated={emailIsActivated}
+        errorMessage={emailErrorMessage}
+        onChange={(event)=>{
+          setEmail(event.target.value)
+        }}
+        onBlur={(event)=>{
+          setEmailIsActivated(email.length>=1 && true)
+        }}
+      />
+      <Registration.Input
+        placeholder="Email address"
+        name="email"
+        value={email}
+        isValid ={emailIsValid}
+        isEmpty = {email.length < 1}
+        isActivated={emailIsActivated}
+        errorMessage={emailErrorMessage}
+        onChange={(event)=>{
+          setEmail(event.target.value)
+        }}
+        onBlur={(event)=>{
+          setEmailIsActivated(email.length>=1 && true)
+        }}
+      />
+      <Registration.Input
+        placeholder="Email address"
+        name="email"
+        value={email}
+        isValid ={emailIsValid}
+        isEmpty = {email.length < 1}
+        isActivated={emailIsActivated}
+        errorMessage={emailErrorMessage}
+        onChange={(event)=>{
+          setEmail(event.target.value)
+        }}
+        onBlur={(event)=>{
+          setEmailIsActivated(email.length>=1 && true)
+        }}
+      />
     </Registration>
   )}
