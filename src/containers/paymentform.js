b00c3e7b6d8e378a06a2c86627861b9e395f60de
okaycodes <fo.okenana@gmail.com>
2021-12-04 23:36:11 +0100

    update paymentform

--- src/containers/paymentform.js
+++ src/containers/paymentform.js
@@ -1,29 +1,9 @@
 import {Registration} from "./../components"
-import * as ROUTES from "./../constants/routes"
-import {inputContext} from  "./../contexts/inputContext"
-import {useContext} from "react"
+// import * as ROUTES from "./../constants/routes"
+
 
 
 export default function PaymentFormContainer(){
-  const {email, setEmail,
-        firstName,firstNameIsActivated,firstNameErrorMessage,
-        handleBlur, handleChange,
-
-        lastName, setLastName,
-        lastNameIsValid, setLastNameIsValid,
-        lastNameIsActivated, setLastNameIsActivated,
-        lastNameErrorMessage,
-
-        cardNumber,
-        cardNumberIsValid, setCardNumberIsValid,
-        cardNumberIsActivated, setCardNumberIsActivated,
-        cardNumberErrorMessage,
-
-        expirationDate,
-        securityCode,
-        emailIsValid,
-        emailIsActivated, setEmailIsActivated,
-        emailErrorMessage} = useContext(inputContext)
 
   return(
     <Registration style={{alignItems: "start"}}>
@@ -36,60 +16,5 @@ export default function PaymentFormContainer(){
         <Registration.IconSecondary src="./../../images/icons/mastercard_icon.png"/>
         <Registration.IconSecondary src="./../../images/icons/icon_verve.png"/>
       </Registration.IconWrapper>
-      <Registration.Input
-        placeholder="First name"
-        name="firstName"
-        value={firstName}
-        isValid ={firstName.length>1}
-        isEmpty = {firstName.length < 1}
-        isActivated={firstNameIsActivated}
-        errorMessage={firstNameErrorMessage}
-        onChange={()=>handleChange}
-        onBlur={()=>handleBlur}
-      />
-      <Registration.Input
-        placeholder="Last name"
-        name="lastName"
-        value={lastName}
-        isValid ={emailIsValid}
-        isEmpty = {email.length < 1}
-        isActivated={emailIsActivated}
-        errorMessage={emailErrorMessage}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-      <Registration.Input
-        placeholder="Card number"
-        name="cardNumber"
-        value={cardNumber}
-        isValid ={cardNumber>1}
-        isEmpty = {cardNumber.length < 1}
-        isActivated={emailIsActivated}
-        errorMessage={emailErrorMessage}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-      <Registration.Input
-        placeholder="Expiration date (MM/YY)"
-        name="expirationDate"
-        value={expirationDate}
-        isValid ={expirationDate > 1}
-        isEmpty = {email.length < 1}
-        isActivated={emailIsActivated}
-        errorMessage={emailErrorMessage}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
-      <Registration.Input
-        placeholder="Security Code (CVV)"
-        name="securityCode"
-        value={securityCode}
-        isValid ={securityCode.length===2 && typeof securityCode==="number"}
-        isEmpty = {securityCode.length < 1}
-        isActivated={emailIsActivated}
-        errorMessage={emailErrorMessage}
-        onChange={handleChange}
-        onBlur={handleBlur}
-      />
     </Registration>
   )}
