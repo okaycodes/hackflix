3dd12a49f8bb6563547d4f13b3380b61a78a21e9
okaycodes <fo.okenana@gmail.com>
2021-12-05 23:19:08 +0100

    update inputContext

--- src/contexts/inputContext.js
+++ src/contexts/inputContext.js
@@ -10,21 +10,15 @@ function InputContextProvider({children}){
   const [email, setEmail] = useState('')
   const [emailIsValid, setEmailIsValid] = useState(true)
   const [emailIsActivated, setEmailIsActivated] = useState(false)
-  // const emailErrorMessage = "Please enter a valid email address"
 
   const [password, setPassword] = useState('')
-  // const [passwordIsValid, setPasswordIsValid] = useState(true)
   const [passwordIsActivated, setPasswordIsActivated] = useState(false)
-  // const passwordErrorMessage = "Password should be between 6 and 60 characters"
 
   useEffect(()=>{
     const emailRegex = /[^@]+@[^@]+\.[^@]+/
     emailIsActivated && setEmailIsValid(emailRegex.test(email))
   }, [email, emailIsActivated])
-  //
-  // useEffect(()=>{
-  //   passwordIsActivated && setPasswordIsValid(password.length>=6)
-  // }, [password, passwordIsActivated])
+
 
   const handleChange=(event)=>{
     const {name, value} = event.target
