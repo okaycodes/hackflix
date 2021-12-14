519cb4f8c510b50856db9796f4ebff499f97f497
okaycodes <fo.okenana@gmail.com>
2021-12-14 20:07:02 +0100

    refactor and update Registration

--- src/components/registration/index.js
+++ src/components/registration/index.js
@@ -1,25 +1,6 @@
-import {Container,
-        Inner,
-        Icon,
-        Image,
-        Title,
-        SubTitle,
-        Text,
-        Link,
-        Label,
-        InputWrapper,
-        Input,
-        Placeholder,
-        InputIcon,
-        ErrorMessage,
-        CheckboxWrapper,
-        Checkbox,
-        List,
-        ListItem,
-        IconSecondary,
-        LinkSecondary,
-        IconWrapper,
-        Modal} from "./styles/registration"
+import {Container, Inner, Icon, Image, Title, SubTitle, Text, ButtonLink,
+        List, ListItem, ListText, ListIcon, SecureIcon, CardsIcon, CardsPaymentLink,
+        IconWrapper} from "./styles/registration"
 
 
 export default function Registration({children, ...restProps}){
@@ -38,10 +19,6 @@ Registration.Icon = function RegistrationIcon({children, ...restProps}){
   return <Icon {...restProps} />
 }
 
-Registration.IconSecondary = function RegistrationIconSecondary({children, ...restProps}){
-  return <IconSecondary {...restProps} />
-}
-
 Registration.IconWrapper = function RegistrationIconWrapper({children, ...restProps}){
   return  <IconWrapper {...restProps}>{children}</IconWrapper>
 }
@@ -58,44 +35,21 @@ Registration.Text = function RegistrationText({children, ...restProps}){
   return <Text {...restProps}>{children}</Text>
 }
 
-Registration.Link = function RegistrationLink({children, ...restProps}){
-  return <Link {...restProps}>{children}</Link>
+Registration.ButtonLink = function RegistrationButtonLink({children, ...restProps}){
+  return <ButtonLink {...restProps}>{children}</ButtonLink>
 }
 
-Registration.LinkSecondary = function RegistrationLinkSecondary({children, ...restProps}){
-  return <LinkSecondary {...restProps}>{children}</LinkSecondary>
+Registration.SecureIcon = function RegistratinSecureIcon({children, ...restProps}){
+  return <SecureIcon {...restProps}>{children}</SecureIcon>
 }
 
-Registration.Input = function RegistrationInput({children,  animated=true, ...restProps}){
-  const {value, isEmpty, isValid, isActivated, placeholder,
-        errorMessage1, errorMessage2, minLength} = {...restProps}
-  return(
-    <InputWrapper {...restProps}>
-      <Label>
-        <Input {...restProps} placeholder={!animated && placeholder} />
-        <Placeholder isEmpty={isEmpty}>{animated && placeholder}</Placeholder>
-        <InputIcon>{children}</InputIcon>
-      </Label>
-      <ErrorMessage>
-      {((isActivated && !isValid) &&  value.length < minLength) ? errorMessage1:
-      (isActivated && !isValid) && errorMessage2}
-      </ErrorMessage>
-    </InputWrapper>
-  )}
+Registration.CardsPaymentLink = function RegistrationCardsPaymentLink({children, ...restProps}){
+  return <CardsPaymentLink {...restProps}>{children}</CardsPaymentLink>
+}
 
-Registration.Checkbox = function RegistrationCheckbox({children, ...restProps}){
-  const {isActive, isChecked, errorMessage, fontSize} = {...restProps}
-  return(
-    <>
-      <CheckboxWrapper> {children}
-        <Checkbox type="checkbox" {...restProps} />
-        <span></span>
-      </CheckboxWrapper>
-      <ErrorMessage fontSize={fontSize}>
-        {isActive && !isChecked && errorMessage}
-      </ErrorMessage>
-    </>
-)}
+Registration.CardsIcon = function RegistrationCardsIcon({children, ...restProps}){
+  return <CardsIcon {...restProps} />
+}
 
 Registration.List = function RegistrationList({children, ...restProps}){
   return <List {...restProps}>{children}</List>
@@ -105,6 +59,10 @@ Registration.ListItem = function RegistrationListItem({children, ...restProps}){
   return <ListItem {...restProps}>{children}</ListItem>
 }
 
-Registration.Modal = function RegistrationModal({children, ...restProps}){
-  return <Modal {...restProps}>{children}</Modal>
+Registration.ListText = function RegistrationListText({children, ...restProps}){
+  return <ListText {...restProps}>{children}</ListText>
+}
+
+Registration.ListIcon = function RegistrationListIcon({children, ...restProps}){
+  return <ListIcon {...restProps}>{children}</ListIcon>
 }
