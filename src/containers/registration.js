f85625ee70ec5bb7e6439e4c02f2a562370f7660
okaycodes <fo.okenana@gmail.com>
2021-12-15 18:42:13 +0100

    refactor and update registrationContainer

--- src/containers/registration.js
+++ src/containers/registration.js
@@ -4,14 +4,14 @@ import * as ROUTES from "./../constants/routes"
 
 export default function RegistrationContainer(){
   return(
-  <Registration maxWidth="340">
+  <Registration maxWidth="340px" textAlign="center" marginTop="125px">
     <Registration.Title>Finish setting up your account</Registration.Title>
-    <Registration.SubTitle style={{textAlign: "center", order: -1}}>
-      step <span style={{fontWeight: "bold"}}>1</span> of <span style={{fontWeight: "bold"}}>3</span>
+    <Registration.SubTitle>
+      step <span>1</span> of <span>3</span>
     </Registration.SubTitle>
     <Registration.Image src="./../../images/icons/devices.png" alt="Devices"/>
-    <Registration.Text style={{alignSelf: "center", textAlign: "center"}}>
-      Netflix is personalized for you. Create a
+    <Registration.Text fontSize="18px">
+      Netflix is personalized for you. <br/> Create a
       password to watch on any device at any time.
     </Registration.Text>
     <Registration.ButtonLink to={`../../${ROUTES.REG_FORM}`}>Next</Registration.ButtonLink>
