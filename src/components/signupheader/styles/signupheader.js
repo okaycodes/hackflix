35c44a15013475b3fbe428bd3b9902fda375c921
okaycodes <fo.okenana@gmail.com>
2021-12-15 18:35:11 +0100

    create and update SignupHeader styles

---
+++ src/components/signupheader/styles/signupheader.js
@@ -0,0 +1,57 @@
+import styled from "styled-components"
+import {Link as ReachRouterLink} from  "react-router-dom"
+
+
+export const Container = styled.div`
+  font-size: 16px;
+  box-sizing: border-box;
+  display: flex;
+  margin: 0 auto;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  background-color: white;
+  padding: 1.5em 2em;
+
+  @media (max-width: 740px) {
+    padding: 1em;
+  }
+  @media (max-width: 500px) {
+    padding: 0 1em;
+  }
+`;
+
+export const Logo = styled.img`
+  height: 45px;
+  width: 167px;
+  margin-right: 40px;
+
+  @media (max-width: 500px) {
+     height: 45px;
+     width: 75px;
+  }
+
+`;
+
+export const Link = styled.p`
+`;
+
+export const ButtonLink = styled(ReachRouterLink)`
+  display: block;
+  height: fit-content;
+  border: 0;
+  cursor: pointer;
+  text-decoration: none;
+  box-sizing: border-box;
+  font-weight: 500;
+  color: black;
+  font-size: 20px;
+
+  &:hover {
+      text-decoration: underline;
+  }
+
+  @media (max-width: 500px) {
+    font-size: 14px;
+  }
+`;
