f4a8b1ff86c1b53cfb1ec753a331680a94e5e2e2
okaycodes <fo.okenana@gmail.com>
2021-12-18 19:42:39 +0100

    upadate signInPage: fix issues with checkbox and add components

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,19 +1,42 @@
-import {useContext} from "react"
+import {useContext, useState} from "react"
 import {Header, SignInForm} from "./../components"
 import {inputContext} from "./../contexts/inputContext"
 import * as REGEX from './../constants/regex';
+import * as ROUTES from './../constants/routes';
+
 
 export default function SignIn({children, ...restProps}){
   const {state, dispatch} = useContext(inputContext)
   const {email, emailIsActive} = state
   const {signInPassword, signInPasswordIsActive} = state
+  const [checkboxIsChecked, setCheckboxIsChecked] = useState(true)
+  const [checkboxIsActive, setCheckboxIsActive] = useState(false)
+  const [checkboxIsHovered, setCheckboxIsHovered] = useState(false)
+
+  const handleChange=()=>{
+      setCheckboxIsChecked(prev=>!prev)
+      setCheckboxIsActive(true)
+    }
+
+  const handleBlur=()=>{!checkboxIsHovered && setCheckboxIsActive(false)}
+/* it was necessary to add the !isHovered condition to the checkbox because
+of a weird behaviour of checkboxes in react where the blur event is called
+whenever the box is clicked (onMouseDown) and then it is focused later causing
+a flash of unneeded behavior. In this case a flash of grey background anytime it
+is clicked before the required white.
+*/
+
+
+  // console.log(checkboxIsActive, checkboxIsChecked)
+
+
   const emailIsValid = REGEX.EMAIL_VALIDATION.test(email)
   const emailIsEmpty = email.length < 1
   const passwordIsValid = signInPassword.length > 6
   const passwordIsEmpty = signInPassword.length < 1
 
   return(
-    <Header>
+    <Header dontShowOnSmallViewPort>
       <Header.Frame>
         <Header.Logo src="./../../images/misc/logo.svg" alt="Netflix" />
       </Header.Frame>
@@ -53,10 +76,37 @@ export default function SignIn({children, ...restProps}){
               dispatch({type: "emptyPassword"}) :
               dispatch({type:"blurred", payload: {passwordIsActive: true}})}
           />
-          <div style={{display: "flex", justifyContent: "space-between"}}>
-            <SignInForm.Checkbox type="checkbox"/>
+          <SignInForm.ButtonLink to="#">Sign In</SignInForm.ButtonLink>
+
+          <SignInForm.FlexBox justifyContent="space-between">
+            <SignInForm.Checkbox
+              type="checkbox"
+              value={checkboxIsChecked}
+              onBlur={handleBlur}
+              onChange={handleChange}
+              onMouseEnter={()=>setCheckboxIsHovered(true)}
+              onMouseLeave={()=>setCheckboxIsHovered(false)}
+              isActive={checkboxIsActive}
+              isHovered={checkboxIsHovered}
+              >Remember me</SignInForm.Checkbox>
             <SignInForm.Link to="#">Need help?</SignInForm.Link>
-          </div>
+          </SignInForm.FlexBox>
+
+          <SignInForm.FlexBox justifyContent="start">
+            <SignInForm.Icon src="./../../images/icons/FB-icon.png"/>
+            <SignInForm.Link to="#" color="grey">Login with Facebook</SignInForm.Link>
+          </SignInForm.FlexBox>
+
+          <SignInForm.FlexBox justifyContent="start">
+            <SignInForm.Text>New to Netflix?</SignInForm.Text>
+            <SignInForm.Link fontSize="16px" color="white" to={`./../${ROUTES.REGISTRATION}`}>
+              Sign up now.</SignInForm.Link>
+          </SignInForm.FlexBox>
+
+          <SignInForm.Text fontSize="13px">
+            This page is protected by Google reCAPTCHA to ensure you're not a bot.
+            <SignInForm.Link to="#" color="#0071eb">Learn more</SignInForm.Link>
+          </SignInForm.Text>
         </SignInForm.Base>
       </SignInForm>
     </Header>
