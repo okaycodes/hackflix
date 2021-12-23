4aa0debdc0dee0938e2b5ad2d1253bd4ffb3dad8
okaycodes <fo.okenana@gmail.com>
2021-12-23 22:47:12 +0100

    create separate file for firebaseContext

--- src/contexts/firebaseContext.js
+++ src/contexts/firebaseContext.js
@@ -1,3 +1,49 @@
-import {createContext} from "react"
+import React, {useReducer} from "react"
 
-export const firebaseContext = createContext(null);
+const firebaseContext = React.createContext(null)
+
+/*===========================
+COME BACK TO THIS handleclick
+=============================*/
+
+function FirebaseContextProvider({children}){
+  const initialSignInPassword = "HHQEyPJ6krf3Lh6"
+  let initialState = {
+    email: "", emailIsActive: false,
+    password: "", passwordIsActive: false,
+    firstName: "", firstNameIsActive: false,
+    lastName: "", lastNameIsActive: false,
+    cardNumber: "", cardNumberIsActive: false,
+    expirationDate: "", expirationDateIsActive: false,
+    securityCode: "", securityCodeIsActive: false,
+    planName: "", planPrice:"",
+    signInPassword: initialSignInPassword, signInPasswordIsActive:false
+  }
+  let [state, dispatch] = useReducer((state, action)=>{
+    switch(action.type){
+      case "input":{
+        return {...state, ...action.payload}
+      }
+      case "blurred":{
+        return {...state, ...action.payload}
+      }
+      case 'savePlan':{
+        return {...state, planName: action.payload.name, planPrice: action.payload.price }
+      }
+      case 'emptyPassword':{
+        return {...state, password: initialSignInPassword}
+      }
+      default:{
+        return state
+      }
+    }
+  }, initialState)
+
+  return (
+    <firebaseContext.Provider value={{state, dispatch}}>
+      {children}
+    </firebaseContext.Provider>
+  )
+}
+
+export {FirebaseContextProvider, firebaseContext}
