61b246961c43a87c78c65d6e82719aef47da42aa
okaycodes <fo.okenana@gmail.com>
2021-12-31 23:43:56 +0100

    update Regex

--- src/constants/regex.js
+++ src/constants/regex.js
@@ -2,3 +2,4 @@ export const EMAIL_VALIDATION = /[^@]+@[^@]+\.[^@]+/
 export const CARD_NUMBER_VALIDATION = /^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/
 export const DATE_VALIDATION = /^(0[1-9]|1[0-2])\/\d{2}$/
 export const CVV_VALIDATION = /^[0-9]{3,4}$/
+export const PHONE_NUMBER_VALIDATION = /^[0-9]{10,16}$/
