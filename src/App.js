f0e4b31d6fccf07f6b4864fbddd73f2815690b6f
okaycodes <fo.okenana@gmail.com>
2022-01-03 14:42:51 +0100

    fixed issues with Routes: moved all Routing to App component

--- src/App.js
+++ src/App.js
@@ -5,20 +5,39 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import SignOut from './pages/SignOut'
 import LoginHelp from './pages/LoginHelp'
+import NotFound from './pages/NotFound'
+import {BlankPage} from "./components"
+
+
 import {SelectContextProvider} from "./contexts/selectContext"
+import RegistrationContainer from "./containers/registration"
+import RegFormContainer from "./containers/regform"
+import RegStepContainer from "./containers/regstep"
+import PlanFormContainer from "./containers/planform"
+import PaymentStepContainer from "./containers/payment"
+import PaymentFormContainer from "./containers/paymentform"
+
+const currentStepUrl = localStorage.getItem('currentStepUrl')
 
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route exact path={ROUTES.HOME} element={<Home />}/>
-        <Route exact path={ROUTES.SIGN_IN} element={<SignIn />}/>
-        <Route exact path={ROUTES.SIGN_UP} element={<SignUp />}/>
-        <Route exact path={ROUTES.SIGN_OUT} element={<SignOut/>}/>
-        <Route exact path={ROUTES.SIGN_IN_HELP}
-        element={<SelectContextProvider><LoginHelp/></SelectContextProvider>}/>
-        <Route exact path={ROUTES.SELECT_PROFILE} element={<LoginHelp/>}/>
+        <Route path={ROUTES.HOME} element={<Home />}/>
+        <Route path={ROUTES.SIGN_IN} element={<SignIn />}/>
+        <Route path="signup" element={<SignUp />}>
+          <Route index element={<RegStepContainer />} />
+          <Route path="registration" element={<RegistrationContainer />}/>
+          <Route path="payment-form" element={currentStepUrl ? <PaymentFormContainer /> : <BlankPage/>}/>
+          <Route path="planform" element={<PlanFormContainer />} />
+          <Route path="payment" element={<PaymentStepContainer />} />
+          <Route path="regform" element={<RegFormContainer />} />
+        </Route>
+        <Route path={ROUTES.SIGN_OUT} element={<SignOut/>}/>
+        <Route path={ROUTES.SIGN_IN_HELP} element={<SelectContextProvider><LoginHelp/></SelectContextProvider>}/>
+        <Route path={ROUTES.SELECT_PROFILE} element={<LoginHelp/>}/>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,6 @@
 import {SignupHeader, Footer} from "./../components"
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, Navigate, Outlet} from 'react-router-dom'
 import RegistrationContainer from "./../containers/registration"
-import RegFormContainer from "./../containers/regform"
-import RegStepContainer from "./../containers/regstep"
-import PlanFormContainer from "./../containers/planform"
-import PaymentStepContainer from "./../containers/payment"
-import PaymentFormContainer from "./../containers/paymentform"
 import {getAuth, onAuthStateChanged} from "firebase/auth"
 import {useState} from "react"
 
@@ -32,29 +27,20 @@ export default function SignUp({children}){
     <>
       <SignupHeader>
           <SignupHeader.Logo src="./../../images/misc/logo.svg" alt="Netflix"/>
-          { isLoggedin ? <SignupHeader.SignOutLink /> : <SignupHeader.SignInLink />}
+          {isLoggedin ? <SignupHeader.SignOutLink /> : <SignupHeader.SignInLink />}
       </SignupHeader>
-
-    <Routes>
-      <Route exact path="/" element={<RegStepContainer />} />
-      <Route exact path="/registration" element={<RegistrationContainer />}/>
-      <Route exact path="/regform" element={<RegFormContainer />} />
-      <Route exact path="/planform" element={<PlanFormContainer />} />
-      <Route exact path="/payment" element={<PaymentStepContainer />} />
-      <Route exact path="/payment-form" element={<PaymentFormContainer />} />
-    </Routes>
-
-    <Footer backgroundColor="#ededed" padding="2em 3em">
-      <Footer.Title>Questions? Contact Us</Footer.Title>
-      <Footer.Row>
-        <Footer.Link href="#">FAQs</Footer.Link>
-        <Footer.Link href="#">Help Center</Footer.Link>
-        <Footer.Link href="#">Terms of Use</Footer.Link>
-        <Footer.Link href="#">Privacy</Footer.Link>
-        <Footer.Link href="#">Cookie Preferences</Footer.Link>
-        <Footer.Link href="#">Corporate Information</Footer.Link>
-      </Footer.Row>
-    </Footer>
+      <Outlet />
+      <Footer backgroundColor="#ededed" padding="2em 3em">
+        <Footer.Title>Questions? Contact Us</Footer.Title>
+        <Footer.Row>
+          <Footer.Link href="#">FAQs</Footer.Link>
+          <Footer.Link href="#">Help Center</Footer.Link>
+          <Footer.Link href="#">Terms of Use</Footer.Link>
+          <Footer.Link href="#">Privacy</Footer.Link>
+          <Footer.Link href="#">Cookie Preferences</Footer.Link>
+          <Footer.Link href="#">Corporate Information</Footer.Link>
+        </Footer.Row>
+      </Footer>
     </>
   )
 }
