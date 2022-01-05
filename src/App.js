ad550f37d7e754f6d27f68ebccb7390067b2725f
okaycodes <fo.okenana@gmail.com>
2022-01-05 11:46:33 +0100

    updated App: rendered App only after auth is completed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import SignUp from './pages/SignUp'
 import SignOut from './pages/SignOut'
 import LoginHelp from './pages/LoginHelp'
 import NotFound from './pages/NotFound'
-import {BlankPage} from "./components"
-
+import {authContext} from "./contexts/authContext"
+import {useContext} from "react"
 
 import {SelectContextProvider} from "./contexts/selectContext"
 import RegistrationContainer from "./containers/registration"
@@ -17,19 +17,19 @@ import PlanFormContainer from "./containers/planform"
 import PaymentStepContainer from "./containers/payment"
 import PaymentFormContainer from "./containers/paymentform"
 
-const currentStepUrl = localStorage.getItem('currentStepUrl')
-
-
 function App() {
-  return (
+const {isAuthenticating} = useContext(authContext)
+
+  return ( 
     <div>
+      { !isAuthenticating &&
       <Routes>
         <Route path={ROUTES.HOME} element={<Home />}/>
         <Route path={ROUTES.SIGN_IN} element={<SignIn />}/>
         <Route path="signup" element={<SignUp />}>
           <Route index element={<RegStepContainer />} />
           <Route path="registration" element={<RegistrationContainer />}/>
-          <Route path="payment-form" element={currentStepUrl ? <PaymentFormContainer /> : <BlankPage/>}/>
+          <Route path="payment-form" element={<PaymentFormContainer />}/>
           <Route path="planform" element={<PlanFormContainer />} />
           <Route path="payment" element={<PaymentStepContainer />} />
           <Route path="regform" element={<RegFormContainer />} />
@@ -38,7 +38,7 @@ function App() {
         <Route path={ROUTES.SIGN_IN_HELP} element={<SelectContextProvider><LoginHelp/></SelectContextProvider>}/>
         <Route path={ROUTES.SELECT_PROFILE} element={<LoginHelp/>}/>
         <Route path="*" element={<NotFound />} />
-      </Routes>
+      </Routes>}
     </div>
   );
 }
