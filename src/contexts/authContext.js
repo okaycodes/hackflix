271c039185a738620480a0da3e6bf8bbbc708f64
okaycodes <fo.okenana@gmail.com>
2022-01-05 11:45:21 +0100

    update authContext created signIn func, updated onAuthStateChanged and wrote logic for isAuthenticating

--- src/contexts/authContext.js
+++ src/contexts/authContext.js
@@ -1,8 +1,9 @@
-import { createContext, useEffect, useState } from "react"
+import { createContext, useEffect, useState, useContext } from "react"
 import {useNavigate} from "react-router-dom"
 import {getFirestore, doc, setDoc, getDoc} from 'firebase/firestore'
-import {getAuth, createUserWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth'
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth'
 import * as ROUTES from "./../constants/routes"
+import { formContext } from "./formContext"
 
 const authContext = createContext(null)
 
@@ -10,13 +11,16 @@ function AuthContextProvider({children}){
     const [user, setUser] = useState(null)
     const [error, setError] = useState("")
     const [currentStepUrl, setCurrentStepUrl] = useState("")
-    const [planName, setPlanName] = useState("")
-    const [planPrice, setPlanPrice] = useState("")
     const [isAuthenticating, setIsAuthenticating] = useState(true)
+    const {dispatch} = useContext(formContext)
+    
     const db = getFirestore()
     const auth = getAuth()
     const navigate = useNavigate()
-
+    
+    useEffect(()=>{
+        user != null && setIsAuthenticating(false)
+    },[user])
     
     const signup=(email, password)=>{
         createUserWithEmailAndPassword(auth, email, password)
@@ -26,7 +30,6 @@ function AuthContextProvider({children}){
             setDoc(doc(db, "users", response.user.uid), {email, password, currentStepUrl: ROUTES.SIGN_UP_HOME})
             navigate(ROUTES.SIGN_UP_HOME)
         }).catch(error=>{
-            console.error(error)
             setError(error.message)
         })
     } 
@@ -37,10 +40,9 @@ function AuthContextProvider({children}){
             getDoc(doc(db, 'users', user.uid)) 
             .then(snapshot =>{
                 if(snapshot.exists()){
-                    const currentUser = snapshot.data()
-                    setCurrentStepUrl(currentUser.currentStepUrl)
-                    setPlanName(currentUser.planName)
-                    setPlanPrice(currentUser.planPrice)
+                    const data = snapshot.data()
+                    setCurrentStepUrl(data.currentStepUrl)
+                    dispatch({type:'savePlan', payload:{name:data.planName, price:data.planPrice}})
                 }
             })
         }else{
@@ -48,15 +50,32 @@ function AuthContextProvider({children}){
         } 
     })
 
-    useEffect(()=>{
-        user != null && setIsAuthenticating(false)
-    })
 
-    const signin=()=>{
-
+    const signin=(email, password)=>{
+        signInWithEmailAndPassword(auth, email, password)
+        .then(response=>{
+            getDoc(doc(db, "users", response.user.uid))
+            .then(snapShot=>{
+                const data = snapShot.data()
+                if(data.currentStepUrl){
+                    setCurrentStepUrl(currentStepUrl)
+                    dispatch({type:'savePlan', payload:{name:data.planName, price:data.planPrice}})
+                    navigate(ROUTES.HOME)
+                } else {
+                    navigate(ROUTES.SELECT_PROFILE)
+                }
+            })
+                // if(error.message===)
+        }).catch(err=>{
+            if(err.message.includes('user-not-found')){
+                setError("unregistered user")
+            }else if (err.message.includes('wrong-password')){
+                setError("incorrect password")
+            }
+        })  
     }
 
-    const update=(mergeObject)=>{
+    const update=(mergeObject, route)=>{
         const userRef = doc(db, 'users', user.uid);
         setDoc(userRef, mergeObject, {merge:true})
         .then(()=>{
@@ -65,8 +84,7 @@ function AuthContextProvider({children}){
                 if(snapshot.exists()){
                     const currentUser = snapshot.data()
                     setCurrentStepUrl(currentUser.currentStepUrl)
-                    setPlanName(currentUser.planName)
-                    setPlanPrice(currentUser.planPrice)
+                    navigate(route)
                 }
             })
         })
